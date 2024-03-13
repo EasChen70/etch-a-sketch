0d269749f268ff1c6a7b9323bb68db316fedd209
EasChen70 <eason_chen@uri.edu>
2024-03-13 00:44:34 -0400

    finished w/ styling and color feature

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const slider = document.querySelector(".slider");
 const clearButton = document.querySelector(".clear");
+const eraser = document.querySelector(".eraser");
 const grid = document.getElementById("grid");
+const colorbutton = document.querySelector(".color");
+const colorpicker = document.getElementById("colorPicker");
+let eraserMode = false;
+let colorMode = true;
 
 function createGrid(gridSize){
     grid.innerHTML = '';
     grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    let label = document.querySelector(".label");
+    label.innerHTML = `${gridSize} x ${gridSize}`
     for(i = 0; i < gridSize ** 2; i++){
         const griditem = document.createElement("div");
         griditem.classList.add('grid-item');
@@ -18,20 +25,35 @@ createGrid(slider.value);
 
 slider.addEventListener('input', function() {
     createGrid(slider.value);
-})
+});
 
 clearButton.addEventListener('click', function() {
     createGrid(slider.value);
 });
 
+colorbutton.addEventListener('click', function(){
+    eraserMode = false;
+    colorMode = true;
+})
 
+eraser.addEventListener('click', function(){
+    eraserMode = true;
+    colorMode = false;
+});
 
+colorpicker.addEventListener('input', function() {
+    eraserMode = false;
+    colorMode = true;
+});
 let mouseDown = false;
-
 
 function changeColor(event){
     if(mouseDown){
-        event.target.style.backgroundColor = '#000000';
+        if(eraserMode){
+            event.target.style.backgroundColor = '#fffefe';
+        }else if(colorMode){
+            event.target.style.backgroundColor = colorpicker.value;
+        }
     }
 }
 
